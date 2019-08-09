e067cbfbd787364be62c0a246c9607cdf7af80a1
RH <incredincomp@gmail.com>
2019-08-08 18:08:41 -0700

    wallpaper setting

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-source ~/.bashrc
+source ~/dotfiles/.bashrc
 sudo apt update && sudo apt upgrade
-sudo apt install git vim
+sudo apt install vim
+
+# set wallpaper attempt number 1
+gsettings set org.gnome.desktop.background picture-uri "file://~/dotfiles/images/wallpaper-1.jpg"

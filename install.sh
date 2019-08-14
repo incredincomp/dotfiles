8daa95e55d898a4c318c86140b927b5753d43420
RH <incredincomp@gmail.com>
2019-08-13 22:44:53 -0700

    added png for grub menu and added atom_install

--- install.sh
+++ install.sh
@@ -40,6 +40,12 @@ chrome_Config () {
     sudo apt install google-chrome-stable -y
 }
 
+install_Atom () {
+    wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+    sudo apt update
+    sudo apt install atom
+}
 java_Config () {
     sudo apt install openjdk-8-jre
 }
